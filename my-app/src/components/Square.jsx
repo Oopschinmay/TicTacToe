df54649aa9e11c4064ea5382f0cc7c1780020571
Oopschinmay <Your GitHub Email>
2024-10-01 01:09:49 +0530

    UpdateTheCommit

---
+++ my-app/src/components/Square.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Square = (props) => {
+    return (
+        <>
+            <div onClick={props.onClick}
+                style={{
+                    border: "1px solid black",
+                    height: "200px",
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }} className='Square'>
+
+                <h5>{props.value}</h5>
+            </div>
+        </>
+    );
+}
+
+export default Square;
+
